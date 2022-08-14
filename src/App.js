8e42f5b564011cb97997812bc5998d989e24406e
Phanindra Duvvuri <phanindra40430@gmail.com>
2022-08-14 11:56:30 +0530

    Add user Card Component.
    completed Challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import { useState } from 'react';
 import StatCard from './components/StatCard';
 import stats from './data.json';
+import userImage from './images/image-jeremy.png';
+
+
 
 function App() {
+  const [currTimeFrame, setCurrTimeFrame] = useState('weekly')
+
+  function getTimeFrame() {
+    if (currTimeFrame === 'daily') return 'Yesterday';
+    if (currTimeFrame === 'weekly') return 'Last Week';
+    if (currTimeFrame === 'monthly') return 'Last Month';
+
+  }
+
+  const cardComponents = stats.map((obj, index) => (
+    <StatCard
+      title={obj.title}
+      currentTime={obj.timeframes[currTimeFrame].current}
+      prevTime={obj.timeframes[currTimeFrame].previous}
+      timeFrame={getTimeFrame()}
+    />
+  ))
+
   return (
     <div className="container">
-      <StatCard stats={stats} />
+      <div className="card user-card">
+        <div className="user__details">
+          <img src={userImage} alt="" className="user__image" />
+          <div>
+            <p className='dull-text'>Report for</p>
+            <p className="user__name">Jeremy Robson</p>
+          </div>
+        </div>
+        <div className="timeframes">
+          <ul>
+            <li><button className={currTimeFrame === 'daily' && 'active'} onClick={() => setCurrTimeFrame('daily')}>Daily</button></li>
+            <li><button className={currTimeFrame === 'weekly' && 'active'} onClick={() => setCurrTimeFrame('weekly')}>Weekly</button></li>
+            <li><button className={currTimeFrame === 'monthly' && 'active'} onClick={() => setCurrTimeFrame('monthly')}>Monthly</button></li>
+          </ul>
+        </div>
+      </div>
+      {cardComponents}
     </div>
   );
 }
--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,22 +1,47 @@
 import ellipsis from '../images/icon-ellipsis.svg';
-import workLogo from '../images/icon-work.svg';
+
+import iconExercise from '../images/icon-exercise.svg';
+import iconPlay from '../images/icon-play.svg';
+import iconSelfCare from '../images/icon-self-care.svg';
+import iconSocial from '../images/icon-social.svg';
+import iconStudy from '../images/icon-study.svg';
+import iconWork from '../images/icon-work.svg';
+
+const colors = {
+    'Work': "hsl(15, 100%, 70%)",
+    'Play': "hsl(195, 74%, 62%)",
+    'Study': "hsl(348, 100%, 68%)",
+    'Exercise': "hsl(145, 58%, 55%)",
+    'Social': "hsl(264, 64%, 52%)",
+    'Self Care': "hsl(43, 84%, 65%)",
+}
+
+const logos = {
+    'Work': iconWork,
+    'Play': iconPlay,
+    'Study': iconStudy,
+    'Exercise': iconExercise,
+    'Social': iconSocial,
+    'Self Care': iconSelfCare,
+}
 
 function StatCard(props) {
-    console.log(props.stats)
+
+    const { title, currentTime, prevTime, timeFrame } = props;
 
     return (
-        <div className="card">
+        <div className="card" style={{ backgroundColor: colors[title] }}>
             <div className="card__image">
-                <img src={workLogo} alt="" />
+                <img src={logos[title]} alt="" />
             </div>
             <div className="card__content">
                 <header>
-                    <p className="card__title">Work</p>
+                    <p className="card__title">{title}</p>
                     <img src={ellipsis} alt="" />
                 </header>
                 <div className="card__stats">
-                    <main>32hrs</main>
-                    <footer>Last Week - 36hrs</footer>
+                    <main>{currentTime}hrs</main>
+                    <footer>{timeFrame} - {prevTime}hrs</footer>
                 </div>
             </div>
         </div>
